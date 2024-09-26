900f0561c6d08aa97cf1afb5beaee65e5669b3b7
Emmanuel Jatto <emmanueljatto33@gmail.com>
2024-09-26 16:55:13 -0400

    edited packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from ''
 
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  
+})
 
 function App() {
   return (
